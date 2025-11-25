54d0be6c3f5e89280f2f0acd79b66022e60ce4bc
JESSICA <jessica.larapiza@edu.sait.ca>
2025-11-25 11:35:29 -0700

    Implement MyStack class with stack operations

--- assignment2StartingCode/src/implementations/MyStack.java
+++ assignment2StartingCode/src/implementations/MyStack.java
@@ -1,5 +1,106 @@
 package implementations;
 
-public class MyStack {
+import java.util.EmptyStackException;
+import utilities.Iterator;
+import utilities.StackADT;
 
+public class MyStack<E> implements StackADT<E> {
+
+    private MyArrayList<E> list;
+
+    public MyStack() {
+        list = new MyArrayList<>();
+    }
+
+    @Override
+    public void push(E toAdd) throws NullPointerException {
+        if (toAdd == null)
+            throw new NullPointerException("Cannot push null onto stack.");
+
+        list.add(toAdd); // add to end = top of stack
+    }
+
+    @Override
+    public E pop() throws EmptyStackException {
+        if (isEmpty())
+            throw new EmptyStackException();
+
+        return list.remove(list.size() - 1);
+    }
+
+    @Override
+    public E peek() throws EmptyStackException {
+        if (isEmpty())
+            throw new EmptyStackException();
+
+        return list.get(list.size() - 1);
+    }
+
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    @Override
+    public E[] toArray(E[] holder) throws NullPointerException {
+        return list.toArray(holder);
+    }
+
+    @Override
+    public boolean contains(E toFind) throws NullPointerException {
+        return list.contains(toFind);
+    }
+
+    @Override
+    public int search(E toFind) {
+        if (toFind == null) return -1;
+
+        // top = last element, position 1
+        for (int i = list.size() - 1, pos = 1; i >= 0; i--, pos++) {
+            if (toFind.equals(list.get(i)))
+                return pos;
+        }
+        return -1;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public boolean equals(StackADT<E> that) {
+        if (that == null || this.size() != that.size()) return false;
+
+        Iterator<E> it1 = this.iterator();
+        Iterator<E> it2 = that.iterator();
+
+        while (it1.hasNext() && it2.hasNext()) {
+            if (!it1.next().equals(it2.next()))
+                return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean stackOverflow() {
+        return false; // no fixed capacity
+    }
 }
+
